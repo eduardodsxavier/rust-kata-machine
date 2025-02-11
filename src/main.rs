5cfeab0691615616d3e23ce095daa1d9ee5ebe44
edsx <eduardodsxavier@gmail.com>
2025-02-11 19:34:15 -0300

    make test for stack

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 mod stack;
+use crate::stack::Stack;
 
 fn main() {
-    let s = stack::new();
+    let s: Stack<u32> = Stack::new();
+
+    if test_stack(s) {
+        print!("working");
+        return
+    }
+
+    print!("not working");
+}
+
+fn test_stack(mut s: Stack<u32>) -> bool {
+    s.push(1);
+
+    if s.peek().unwrap() != 1 { return false }
+
+    s.pop();
+
+    if s.peek().is_some() { return false }
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    if s.peek().unwrap() != 3 { return false }
+
+    s.push(4);
+    s.push(5);
+
+    s.pop();
+
+    if s.peek().unwrap() != 4 { return false }
+
+    return true
 }
