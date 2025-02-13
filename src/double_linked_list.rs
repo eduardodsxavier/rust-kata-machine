1469da23dcf3b7519313ec7d07f73b84e0359ab4
edsx <eduardodsxavier@gmail.com>
2025-02-13 19:12:14 -0300

    implement remove and insert_at at double linked list

--- src/double_linked_list.rs
+++ src/double_linked_list.rs
@@ -1,6 +1,6 @@
-use std::ops::Deref;
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::usize;
 
 
 struct Node<T> {
@@ -40,7 +40,40 @@ impl<T> DoubleLinkedList<T> {
         self.head = Some(Rc::clone(&node));
     }
 
-    fn insert_at(&mut self) {}
+    fn insert_at(&mut self, index: usize, value: T) {
+        if index == 0 {
+            self.prepend(value);
+            return
+        }
+
+        if index == self.lenght {
+            self.append(value);
+            return 
+        }
+
+        if index > self.lenght {
+            return
+        }
+
+        self.lenght += 1;
+
+        let node = Rc::new(RefCell::new(Node {value, prev: None, next:None}));
+
+        let mut node_p: Option<Rc<RefCell<Node<T>>>> = Some(Rc::clone(&self.head.clone().unwrap()));
+        for _ in 0..index {
+            node_p = Some(Rc::clone(&node_p.unwrap().borrow_mut().next.clone().unwrap()));
+        }
+
+        if node_p.is_none() {
+            return
+        }
+
+        node.borrow_mut().prev = Some(Rc::clone(&node_p.clone().unwrap()));
+        node.borrow_mut().prev.clone().unwrap().borrow_mut().next = Some(Rc::clone(&node.clone()));
+
+        node.borrow_mut().next = Some(Rc::clone(&node_p.clone().unwrap().borrow_mut().next.clone().unwrap()));
+        node.borrow_mut().next.clone().unwrap().borrow_mut().prev = Some(Rc::clone(&node));
+    }
 
     fn append(&mut self, value: T) {
         self.lenght += 1;
@@ -58,7 +91,50 @@ impl<T> DoubleLinkedList<T> {
         self.tail = Some(Rc::clone(&node));
     }
 
-    fn remove(&mut self) {}
+    fn remove(&mut self, value: T) -> Option<T> 
+        where 
+            T: PartialEq + Clone
+    {
+        let mut node_p: Option<Rc<RefCell<Node<T>>>> = Some(Rc::clone(&self.head.clone().unwrap()));
+        for _ in 0..self.lenght {
+            if node_p.clone().unwrap().borrow_mut().value == value {
+                break;
+            }
+            node_p = Some(Rc::clone(&node_p.unwrap().borrow_mut().next.clone().unwrap()));
+        }
+
+        if node_p.is_none() {
+            return None
+        }
+
+        self.lenght -= 1;
+
+        let value = node_p.clone().unwrap().borrow_mut().value.clone();
+
+        if node_p.clone().unwrap().borrow_mut().prev.is_none() {
+            if node_p.clone().unwrap().borrow_mut().next.is_none() {
+                self.head = None;
+                self.tail = None;
+                return Some(value)
+            }
+            self.head = node_p.clone().unwrap().borrow_mut().next.clone();
+            node_p.clone().unwrap().borrow_mut().next.clone().unwrap().borrow_mut().prev = None;
+            return Some(value)
+        }
+
+        if node_p.clone().unwrap().borrow_mut().next.is_none() {
+            node_p.clone().unwrap().borrow_mut().prev.clone().unwrap().borrow_mut().next = None;
+            self.tail = node_p.clone().unwrap().borrow_mut().prev.clone();
+        }
+
+
+        node_p.clone().unwrap().borrow_mut().next.clone().unwrap().borrow_mut().prev =
+            node_p.clone().unwrap().borrow_mut().prev.clone();
+        node_p.unwrap().borrow_mut().prev.clone().unwrap().borrow_mut().next =
+            node_p.clone().unwrap().borrow_mut().next.clone();
+
+        return Some(value)
+    }
 
     fn get(&mut self, index: usize) -> Option<T>
     where 
